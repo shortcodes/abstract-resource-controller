a528c62d6762f2a8fa29123427bcbf950e24dc46
roman.szymanski <r.szymanski@mindz.it>
2019-02-07 15:10:24 +0100

    Create package provider

--- src/AbstractResourceControllerProvider.php
+++ src/AbstractResourceControllerProvider.php
@@ -2,32 +2,12 @@
 
 namespace Shortcodes\Jwt;
 
-use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\ServiceProvider;
-use Shortcodes\Jwt\Services\Auth\JwtGuard;
 
-class JwtServiceProvider extends ServiceProvider
+class AbstractResourceControllerProvider extends ServiceProvider
 {
-    /**
-     * Register services.
-     *
-     * @return void
-     */
     public function register()
     {
-        //
-    }
-
-    /**
-     * Bootstrap services.
-     *
-     * @return void
-     */
-    public function boot()
-    {
-        Auth::extend('jwt', function ($app, $name, array $config) {
-            $provider = Auth::createUserProvider($config['provider'] ?? null);
-            return new JwtGuard($provider, $app->request);
-        });
+        $this->app->make('Shortcodes\AbstractResourceController\Controllers\AbstractResourceControllerController');
     }
 }
